910ea97fd9bf4b99e23c3b0f76af86a918e557a6
JinYoung Kim <jykim0827@gmail.com>
2025-06-29 23:50:57 +0900

    Add back part

---
+++ Back/src/main/java/store/volanote/Vola/Note/VolaNoteApplication.java
@@ -0,0 +1,13 @@
+package store.volanote.Vola.Note;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolaNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolaNoteApplication.class, args);
+	}
+
+}
---
+++ Back/src/test/java/store/volanote/Vola/Note/VolaNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package store.volanote.Vola.Note;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolaNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
